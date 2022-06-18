d92d88709fe7cfad437027c20c09d106797756cd
Chance Bridges <chance.bridges1998@gmail.com>
2022-06-18 17:32:32 -0400

    initial commit

---
+++ dictionaries/6_2_favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'alpha' : 1 ,
+    'beta' : 2,
+    'gamma' : 3,
+    'delta' : 4,
+    'epsilon' : 5
+    }
+
+print("Alpha's favorite number is " + str(favorite_numbers['alpha']))
+print("Beta's favorite number is " + str(favorite_numbers['beta']))
+print("Gamma's favorite number is " + str(favorite_numbers['gamma']))
+print("Delta's favorite number is " + str(favorite_numbers['delta']))
+print("Epsilon's favorite number is " + str(favorite_numbers['epsilon']))

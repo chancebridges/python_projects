a681ed5cf68b15ac0bce6d93be3e651d7b104945
Nick Connors <naconnors@outlook.com>
2022-06-17 21:04:27 -0400

    Refactor calculator and comments about areas of improvement

--- projects/calculator_2.py
+++ projects/calculator_2.py
@@ -1,28 +1,57 @@
-print("Please input one of the following: ")
-print("add, subtract, multiply, divide, square")
-print("if doing a square, the first number will be used.")
+# Simple calculator
+# Areas for improvement
+# 1. Extend to accept floats instead of just int
+# 2. Don't exit after first operation, allow for several 
+# 3. Add tests
 
-kind = input("What would you like to do (add): ")
-num_1 = input("first number: ")
-num_2 = input("second number: ")
+print("Command Line Calculator")
+print("------------------------")
+print("Supports 'add', 'subtract', 'multiply' ")
+print(" 'divide', 'square' and 'exponent'. ")
 
-number_1 = int(num_1)
-number_2 = int(num_2)
+valid_operators = ['add', 'subtract', 'multiply', 'divide', 'square', 'exponent']
 
-# A new comment
-if kind == "add":
-    print(number_1 + number_2)
-if kind == "subtract":
-    print(number_1 - number_2)
-if kind == "multiply":
-    print(number_1 * number_2)
-if kind == "divide":
-    print(number_1 / number_2)
-if kind == "square":
-    print(number_1 ** 2)
-if kind == "mod":
-    print(number_1 % number_2)
-else:
-    # Assume user wanted addition
-    print(number_1 + number_2)
+def main():
 
+    operator = ""
+    while True:
+        operator = input("Enter an operator: ")
+        if operator in valid_operators:
+            break
+        print("Enter a valid operator.")
+
+    operand_1 = ""
+    while True:
+        operand_1 = input("Enter first number: ")
+        if operand_1.isnumeric():
+            operand_1 = int(operand_1)
+            break
+        print("Enter a valid number.")
+
+    if operator != "square":
+        operand_2 = ""
+        while True:
+            operand_2 = input("Enter second number: ")
+            if operand_2.isnumeric():
+                operand_2 = int(operand_2)
+                break
+            print("Enter a valid number.")
+
+    print("Result: ", end='')
+    if operator == "add":
+        print(operand_1 + operand_2)
+    elif operator == "subtract":
+        print(operand_1 - operand_2)
+    elif operator == "multiply":
+        print(operand_1 * operand_2)
+    elif operator == "divide":
+        print(operand_1 / operand_2)
+    elif operator == "square":
+        print(operand_1 ** 2)
+    elif operator == "exponent":
+        print(operand_1 ** operand_2)
+    else:
+        print(operand_1 + operand_2)
+
+if __name__ == "__main__":
+    main()

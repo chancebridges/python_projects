33667b83a557ac07839012d03943090c76d7bf31
Chance Bridges <chance.bridges1998@gmail.com>
2022-06-19 10:28:08 -0400

    restructure

--- learning_python/dictionaries/looping_through_all_the_keys_in_a_dictionary.py
+++ learning_python/dictionaries/looping_through_all_the_keys_in_a_dictionary.py
@@ -9,4 +9,10 @@ favorite_languages = {
 
 for name in favorite_languages.keys():
     print(name.title())
-    
+# line 10 tells python to pull all the keys from the dictionary and store them
+# one at a time in the variable name. 
+
+# looping through the keys in the default behavior when looping through the dictionary
+
+# this code for line 10 could have been " for name in favorite_languages: "
+# and it would have given the same result. 
--- learning_python/dictionaries/message_from_a_dictionary.py
+++ learning_python/dictionaries/message_from_a_dictionary.py
@@ -13,3 +13,7 @@ for name in favorite_languages.keys():
         print(" Hi " + name.title() + 
         ", I see your favorite language is " +
         favorite_languages[name].title() + "!")
+
+# at line 8 we make a list of friends that we want to print a message to.
+# at line 12 we check to see if the name is in the list friends
+
